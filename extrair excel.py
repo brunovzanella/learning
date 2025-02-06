c3cfff4c64e2cc41b8a61066407547756f92b0b0
brunovzanella <brunovzanella@gmail.com>
2025-02-06 14:46:13 -0300

    Add files via upload
    continuar em casa

---
+++ extrair excel.py
@@ -0,0 +1,23 @@
+import pyodbc
+import pandas as pd
+
+server = 'INSERIR IP DO BANCO'
+database = 'INSERIR BASE DE DADOS'
+username = 'NOME DE USUÁRIO'
+password = 'SENHA'
+
+conn = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+                      f'SERVER={server};'
+                      f'DATABASE={database};'
+                      f'UID={username};'
+                      f'PWD={password}'
+)
+query = 'INSERIR QUERY'
+
+df = pd.read_sql(query, conn)
+
+conn.close()
+
+df.to_excel(r'C:\Users\BrunoVieiraZanella\Desktop\teste\dados_banco.xlsx', index=False)
+
+print('Planilha salva com sucesso')
